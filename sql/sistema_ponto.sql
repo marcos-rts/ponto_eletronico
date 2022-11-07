42663406218bfa0cebbab42e8bb07ca5a6779909
Marcos Alexandre <reis.marcos43@gmail.com>
2022-11-07 19:52:51 -0300

    Adição SQL

---
+++ sql/sistema_ponto.sql
@@ -0,0 +1,144 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Tempo de geração: 04-Nov-2022 às 05:11
+-- Versão do servidor: 10.4.24-MariaDB
+-- versão do PHP: 8.1.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Banco de dados: `sistema_ponto`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `pontos`
+--
+
+CREATE TABLE `pontos` (
+  `id` int(20) NOT NULL,
+  `data_entrada` date NOT NULL,
+  `entrada` time NOT NULL,
+  `saida` time NOT NULL,
+  `usuario_id` int(20) NOT NULL,
+  `projeto_id` int(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `projetos`
+--
+
+CREATE TABLE `projetos` (
+  `id` int(11) NOT NULL,
+  `nome` varchar(220) NOT NULL,
+  `descricao` varchar(500) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Extraindo dados da tabela `projetos`
+--
+
+INSERT INTO `projetos` (`id`, `nome`, `descricao`) VALUES
+(1, 'Controle de equipamento', 'CRUD feito em PHP e Mysql para controle de entrada e saida de equipamento.'),
+(2, 'Vulcan', 'Site desenvolvido para empresa ficticia de nome Vulcan para projeto da Faculdade Fatec Campinas O site é desenvolvido com um molde do Framework Bulma, com modificações para o projeto.'),
+(3, 'Ponto Eletronico', 'Projeto de ponto eletronico e analise de rpojetos em andamento para a MS Tecnology');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `usuarios`
+--
+
+CREATE TABLE `usuarios` (
+  `id` int(20) NOT NULL,
+  `nome` varchar(220) NOT NULL,
+  `cod_login` int(20) DEFAULT NULL,
+  `cpf` varchar(100) DEFAULT NULL,
+  `3cpf` int(20) DEFAULT NULL,
+  `senha` int(11) DEFAULT NULL,
+  `ativo` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Extraindo dados da tabela `usuarios`
+--
+
+INSERT INTO `usuarios` (`id`, `nome`, `cod_login`, `cpf`, `3cpf`, `senha`, `ativo`) VALUES
+(1, 'Marcos Alexandre Reis Torquato dos Santos', 17783, '44889394818', 448, NULL, 1),
+(2, 'Gabriel Oseas Santiago de Souza', 27183, '49351555828', 493, NULL, 0);
+
+--
+-- Índices para tabelas despejadas
+--
+
+--
+-- Índices para tabela `pontos`
+--
+ALTER TABLE `pontos`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `projeto_id` (`projeto_id`),
+  ADD KEY `usuario_id` (`usuario_id`);
+
+--
+-- Índices para tabela `projetos`
+--
+ALTER TABLE `projetos`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Índices para tabela `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT de tabelas despejadas
+--
+
+--
+-- AUTO_INCREMENT de tabela `pontos`
+--
+ALTER TABLE `pontos`
+  MODIFY `id` int(20) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `projetos`
+--
+ALTER TABLE `projetos`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT de tabela `usuarios`
+--
+ALTER TABLE `usuarios`
+  MODIFY `id` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- Restrições para despejos de tabelas
+--
+
+--
+-- Limitadores para a tabela `pontos`
+--
+ALTER TABLE `pontos`
+  ADD CONSTRAINT `pontos_ibfk_1` FOREIGN KEY (`projeto_id`) REFERENCES `projetos` (`id`),
+  ADD CONSTRAINT `pontos_ibfk_2` FOREIGN KEY (`usuario_id`) REFERENCES `usuarios` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
